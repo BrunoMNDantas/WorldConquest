6dc7c18c7d32961c43ac1ab2089100df79e4adbd
BMNDantas <bmndantas@gmail.com>
2022-02-25 20:46:48 +0000

    RegionDetail component added

--- src/components/game/info/Info.js
+++ src/components/game/info/Info.js
@@ -1,17 +1,21 @@
 import RegionsInfo from "./regionsInfo/RegionsInfo";
 import AttacksInfo from "./attacksInfo/AttacksInfo";
 import styles from './Info.module.css'
+import RegionDetail from "./regionDetail/RegionDetail";
 
 const Info = () => {
     console.log(":Info::")
 
     return (
         <div className={styles.root}>
-            <div className={styles.panel}>
+            <div className={styles.regionsInfo}>
                 <RegionsInfo/>
             </div>
-            <div className={styles.panel}>
+            <div className={styles.attacksInfo}>
                 <AttacksInfo/>
+            </div>
+            <div className={styles.regionDetail}>
+                <RegionDetail/>
             </div>
         </div>
     )
---
+++ src/components/game/info/regionDetail/RegionDetail.js
@@ -0,0 +1,56 @@
+import { useSelector } from "react-redux";
+import style from './RegionDetail.module.css'
+import { Slide, Paper, Divider, ListItem, ListItemAvatar, ListItemText } from "@material-ui/core";
+import Person from "@material-ui/icons/Person";
+
+import { selectRegionById } from "../../../../store/region/Region.selectors";
+import { selectSelectedRegionId } from "../../../../store/game/Game.selectors";
+import { selectPlayerById } from "../../../../store/player/Player.selectors";
+import { selectCountryById } from "../../../../store/country/Country.selectors";
+
+
+const RegionDetail = () => {
+    console.log("::Region Detail::")
+
+    const selectedRegionId = useSelector(selectSelectedRegionId);
+    const region = useSelector(state => selectRegionById(state, selectedRegionId))
+    const player = useSelector(state => selectPlayerById(state, region?.playerId))
+    const playerInitialCountry = useSelector(state => selectCountryById(state, player?.initialCountry))
+    const country = useSelector(state => selectCountryById(state, region?.countryId))
+    const open = selectedRegionId !== undefined && selectedRegionId !== null
+
+    return (
+        <Slide direction="up" in={open} mountOnEnter unmountOnExit>
+            <Paper elevation={10} className={style.paper}>
+                <div className={style.infoContainer}>
+                    <ListItem>
+                        <ListItemAvatar disablePadding>
+                            <img src={country?.flagUrl} className={style.image}/>
+                        </ListItemAvatar>
+                        <ListItemText primary={country?.name} secondary={country?.capital.name} disablePadding/>
+                    </ListItem>
+                    <ListItem>
+                        <ListItemAvatar disablePadding>
+                            <img src={playerInitialCountry?.flagUrl} className={style.image}/>
+                        </ListItemAvatar>
+                        <ListItemText primary={player?.name} disablePadding/>
+                    </ListItem>
+                    <ListItem>
+                        <ListItemAvatar disablePadding>
+                            <Person fontSize="large"/>  
+                        </ListItemAvatar>
+                        <ListItemText primary={"Units"} secondary={region?.units} disablePadding/>
+                    </ListItem>
+                </div>
+                <Divider orientation="vertical" flexItem light/>
+                <div className={style.actionsContainer}>
+                    <div></div>
+                    <div></div>
+                    <div></div>
+                </div>
+            </Paper>
+        </Slide>
+    )
+}
+
+export default RegionDetail;
