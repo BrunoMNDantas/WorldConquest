3d602fbae77c8d32317faf96b174fea2d58af993
BMNDantas <bmndantas@gmail.com>
2022-02-28 20:29:29 +0000

    MovesInfo refactored

--- src/components/game/info/movesInfo/playerItem/PlayerItem.js
+++ src/components/game/info/movesInfo/playerItem/PlayerItem.js
@@ -7,56 +7,10 @@ import { ExpandLess, ExpandMore } from '@material-ui/icons';
 
 import { selectCurrentPlayerId } from '../../../../../store/game/Game.selectors'
 import { selectPlayerById } from '../../../../../store/player/Player.selectors';
-import { selectAllMoves, selectMoveById } from '../../../../../store/move/Move.selectors' 
-import { selectRegionsOfPlayer, selectRegionById } from '../../../../../store/region/Region.selectors';
-import { selectCountryById } from '../../../../../store/country/Country.selectors';
-import { calculateDistance } from '../../../../../services/game/MovesUpdater';
+import { selectAllMoves  } from '../../../../../store/move/Move.selectors' 
+import { selectRegionsOfPlayer } from '../../../../../store/region/Region.selectors';
 
-const Item = ({playerId, moveId}) => {
-    const move = useSelector(state => selectMoveById(state, moveId))
-    const fromRegion = useSelector(state => selectRegionById(state, move.fromRegionId))
-    const toRegion = useSelector(state => selectRegionById(state, move.toRegionId))
-    const fromCountry = useSelector(state => selectCountryById(state, fromRegion.countryId))
-    const toCountry = useSelector(state => selectCountryById(state, toRegion.countryId))
-    const fromPlayer = useSelector(state => selectPlayerById(state, move.fromPlayerId))
-    const toPlayer = useSelector(state => selectPlayerById(state, toRegion.playerId))
-
-    const distance = Math.round(calculateDistance(move.position, toCountry.capital.center))
-    const isFromPlayer = move.fromPlayerId === playerId
-
-    let fromContent = <div className={style.fromContainer}>
-        <img src={fromPlayer.avatar} className={style.image} style={{borderColor:fromPlayer.color}}/>
-        <img src={fromCountry.flagUrl} className={style.itemFromFlag}/>
-    </div>
-
-    let distanceContent = <div className={style.distanceContainer}>
-        <div className={style.itemLineDistance}>
-            {distance} Km
-        </div>
-        <div className={isFromPlayer ? style.itemLineFrom : style.itemLineTo}/>
-    </div>
-     
-    let toContent = <div class={style.toContainer}>
-        <img src={toPlayer.avatar} className={style.image} style={{borderColor:toPlayer.color}}/>
-        <img src={toCountry.flagUrl} className={style.itemToFlag}/>
-    </div>
-
-    return (
-        <ListItem className={style.item}>
-            <div className={style.itemContent}>
-                <div className={style.fromContent}>
-                    {fromContent}
-                </div>
-                <div className={style.distanceContent}>
-                    {distanceContent}
-                </div>
-                <div className={style.toContent}>
-                    {toContent}
-                </div>
-            </div>
-        </ListItem>
-    )  
-}
+import MoveItem from './moveItem/MoveItem';
 
 const PlayerItem = ({playerId}) => {
     console.log(":Move Item::")
@@ -91,7 +45,7 @@ const PlayerItem = ({playerId}) => {
             </ListItem>
             <Collapse in={open} timeout="auto" unmountOnExit>
                 <List component="div" disablePadding>
-                    { playerMoves.map(moveId => <Item key={"MoveItem " + moveId} playerId={playerId} moveId={moveId}/>) }
+                    { playerMoves.map(moveId => <MoveItem key={"MoveItem " + moveId} playerId={playerId} moveId={moveId}/>) }
                 </List>
             </Collapse>
         </div>
---
+++ src/components/game/info/movesInfo/playerItem/moveItem/MoveItem.js
@@ -0,0 +1,68 @@
+import React from 'react'
+import { useSelector } from 'react-redux';
+import style from './MoveItem.module.css'
+
+import { ListItem, Tooltip } from '@material-ui/core';
+
+import { selectPlayerById } from '../../../../../../store/player/Player.selectors';
+import { selectMoveById } from '../../../../../../store/move/Move.selectors' 
+import { selectRegionById } from '../../../../../../store/region/Region.selectors';
+import { selectCountryById } from '../../../../../../store/country/Country.selectors';
+import { calculateDistance } from '../../../../../../services/game/MovesUpdater';
+
+const MoveItem = ({playerId, moveId}) => {
+    const move = useSelector(state => selectMoveById(state, moveId))
+    const fromRegion = useSelector(state => selectRegionById(state, move.fromRegionId))
+    const toRegion = useSelector(state => selectRegionById(state, move.toRegionId))
+    const fromCountry = useSelector(state => selectCountryById(state, fromRegion.countryId))
+    const toCountry = useSelector(state => selectCountryById(state, toRegion.countryId))
+    const fromPlayer = useSelector(state => selectPlayerById(state, move.fromPlayerId))
+    const toPlayer = useSelector(state => selectPlayerById(state, toRegion.playerId))
+
+    const distance = Math.round(calculateDistance(move.position, toCountry.capital.center))
+    const isJoin = toPlayer?.id === fromPlayer.id
+    const isAttacking = !isJoin && fromPlayer.id === playerId
+    
+    const leftCountry = fromPlayer.id === playerId ? fromCountry : toCountry;
+    const rightCountry = fromPlayer.id === playerId ? toCountry : fromCountry;
+    const leftPlayer = fromPlayer.id === playerId ? fromPlayer : toPlayer;
+    const rightPlayer = fromPlayer.id === playerId ? toPlayer : fromPlayer;
+
+    let leftContent = 
+        <div className={style.leftContent}>
+            <img src={leftPlayer.avatar} className={style.leftPlayerAvatar} style={{borderColor:leftPlayer.color}}/>
+            <img src={leftCountry.flagUrl} className={style.leftCountryFlag}/>
+        </div>
+
+    let distanceContent = 
+        <div className={style.distanceContent}>
+            <div className={style.distance}>
+                {distance} Km
+            </div>
+            <div className={isJoin ? style.joinLine : (isAttacking ? style.attackingLine : style.underAttackLine)}/>
+        </div>
+     
+    let rightContent = 
+        toPlayer ? 
+        <div className={style.rightContent}>
+            <img src={rightCountry.flagUrl} className={style.rightCountryFlag}/>
+            <img src={rightPlayer.avatar} className={style.rightPlayerAvatar} style={{borderColor:rightPlayer.color}}/>
+        </div>
+        :
+        <div className={style.rightContent}>
+            <img src={toCountry.flagUrl} className={style.rightCountryFlag}/>
+            <div className={style.noPlayerAvatar} />
+        </div>
+        
+    return (
+        <Tooltip title={isAttacking ? "Attacking" : (isJoin ? "Joining Units" : "Under Attack")} placement="right">
+            <ListItem className={style.root}>
+                {leftContent}
+                {distanceContent}
+                {rightContent}
+            </ListItem>
+        </Tooltip>
+    )  
+}
+
+export default MoveItem;
